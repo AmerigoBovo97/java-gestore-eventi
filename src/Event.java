82b6d61a020e351b97bb3262be81419162688518
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-15 14:51:09 +0100

    feat: add constructor to Event class

--- src/Event.java
+++ src/Event.java
@@ -1,3 +1,16 @@
 public class Event {
     
+
+    private String title;
+    private String date;
+    private int totalSeats;
+    private int bookedSeats = 0; //initially evry event will always have 0 booked seats
+
+    public void Event(String title, String date, int totalSeats){
+        this.title = title;
+        this.date = date;
+        this.totalSeats = totalSeats;
+    }
+
+
 }
