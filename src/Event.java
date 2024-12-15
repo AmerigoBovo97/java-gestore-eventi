4a2b5524be0d4fa169fbbd8644e1d652ba05a66a
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-15 15:19:32 +0100

    feat: add LocalDate to Event.date and set Event.totalSeats to final

--- src/Event.java
+++ src/Event.java
@@ -1,14 +1,16 @@
+import java.time.LocalDate;
+
 public class Event {
     
 
     private String title;
-    private String date;
-    private int totalSeats;
+    private LocalDate date;
+    private final int totalSeats;
     private int bookedSeats = 0; //initially evry event will always have 0 booked seats
 
-    public Event(String title, String date, int totalSeats){
+    public Event(String title, int year, int month, int dayOfMonth, int totalSeats){
         this.title = title;
-        this.date = date;
+        this.date = LocalDate.of(year, month, dayOfMonth);
         this.totalSeats = totalSeats;
     }
 
@@ -20,12 +22,12 @@ public class Event {
         this.title = title;
     }
 
-    public String getDate(){
+    public LocalDate getDate(){
         return this.date;
     }
 
-    public void setDate(String date){
-        this.date = date;
+    public void setDate(int year, int month, int dayOfMonth){
+        this.date = LocalDate.of(year, month, dayOfMonth);
     }
     public int getTotalSeats(){
         return this.totalSeats;
