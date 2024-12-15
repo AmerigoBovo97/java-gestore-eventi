4734db2c6c8e31b444fd663b7793027de27e4b48
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-15 14:58:21 +0100

    quick fix: remove void from constructor definition

--- src/Event.java
+++ src/Event.java
@@ -6,7 +6,7 @@ public class Event {
     private int totalSeats;
     private int bookedSeats = 0; //initially evry event will always have 0 booked seats
 
-    public void Event(String title, String date, int totalSeats){
+    public Event(String title, String date, int totalSeats){
         this.title = title;
         this.date = date;
         this.totalSeats = totalSeats;
