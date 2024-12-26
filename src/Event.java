21b3c6c20b9f01e17c48b66150374d3fafd2cefe
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-26 15:34:39 +0100

    feat: change Event class to abstract

--- src/Event.java
+++ src/Event.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-public class Event {
+public abstract class Event {
     
 
     private String title;
